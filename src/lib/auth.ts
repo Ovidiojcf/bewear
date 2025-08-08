7fa984c179cb8dd34ff033f44be9ade2fe4c993e
Ovidiojcf <ojcfarias@gmail.com>
2025-08-07 22:08:30 -0300

    chore: add better-auth setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,9 +3,11 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { db } from "@/db";
 
-
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg",
-    }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
 });
