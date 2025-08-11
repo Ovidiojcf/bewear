bc7d30976c4d49aadcec26e2cd202d849b9d13c3
Ovidiojcf <ojcfarias@gmail.com>
2025-08-11 17:38:18 -0300

    feat: add dynamic class supoort using prop an cn utility

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -3,25 +3,34 @@ import Link from "next/link";
 
 import { formatCentsToBrl } from "@/app/helpers/handle-price";
 import { productTable, productVariantTable } from "@/db/schema";
+import { cn } from "@/lib/utils";
 
 interface ProductItemProps {
   product: typeof productTable.$inferSelect & {
     variants: (typeof productVariantTable.$inferSelect)[];
   };
+  textContainerClassName?: string;
+  imageClassName?: string;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
   const firstVariant = product.variants[0];
   return (
     <Link href="/product">
       <Image
         src={firstVariant.imageUrl}
         alt={firstVariant.name}
-        width={150}
-        height={150}
-        className="rounded-3xl"
+        sizes="100vh"
+        width={0}
+        height={0}
+        className="h-full w-full rounded-3xl"
       />
-      <div className="flex max-w-[150px] flex-col gap-1">
+      <div
+        className={cn(
+          "flex max-w-[150px] flex-col gap-1",
+          textContainerClassName,
+        )}
+      >
         <p className="truncate text-sm font-medium">{product.name}</p>
         <p className="text-muted-foreground truncate text-xs font-medium">
           {product.description}
